15b484eeca2fa0359b6daea59f3486c850601549
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2023-07-28 23:23:38 -0300

    Modulo 2: Aula 2 Salvando em memoria

--- fundamentos-node/src/server.js
+++ fundamentos-node/src/server.js
@@ -1,13 +1,26 @@
 import http from "http";
 
+
+const users = [];
+
+
 const server = http.createServer((req, res) => {
     const { url, method } = req;
 
-    if(method === "GET" && url === '/users'){
-        return res.end('Listagem de usuarios');    
+    if (method === "GET" && url === '/users') {
+        return res
+            .setHeader('Content-Type', 'application/json')
+            .end(JSON.stringify(users));
     }
 
-    if(method === "POST" && url === '/users'){
+    if (method === "POST" && url === '/users') {
+        const id = users.length + 1;
+        users.push({
+            id,
+            name: `Usuario ${id}`,
+            email: `usuario${id}@example.com`
+        })
+
         return res.end('Criação de usuario');
     }
 
