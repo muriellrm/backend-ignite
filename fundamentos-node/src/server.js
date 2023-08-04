9269f5d82fdc7dfb1975f9b73cf6a4652a6fe948
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2023-08-03 23:11:47 -0300

    using buffer stream to fetch req body

--- fundamentos-node/src/server.js
+++ fundamentos-node/src/server.js
@@ -1,11 +1,22 @@
 import http from "node:http";
 
-
 const users = [];
 
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { url, method } = req;
+
+    const buffers = [];
+
+    for await (const chunck of req) {
+        buffers.push(chunck);
+    }
+
+    try {        
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        req.body = null;
+    }
 
     if (method === "GET" && url === '/users') {
         return res
@@ -14,11 +25,12 @@ const server = http.createServer((req, res) => {
     }
 
     if (method === "POST" && url === '/users') {
+        const { name, email } = req.body;
         const id = users.length + 1;
         users.push({
             id,
-            name: `Usuario ${id}`,
-            email: `usuario${id}@example.com`
+            name,
+            email
         })
 
         return res.writeHead(201).end();
