a93474f3f9956ceb81f2906a809bb1010e2c4e3b
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2023-11-02 21:31:57 -0300

    feat: including gyms repository on checkin service

--- api-solid/src/services/check-in.spec.ts
+++ api-solid/src/services/check-in.spec.ts
@@ -1,16 +1,31 @@
 import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest'
-
-import { FakeCheckInRepository } from '#/repositories/fakes/fake-check-ins-repositories'
 import { randomUUID } from 'node:crypto'
+
 import { CheckInService } from './check-in'
 
+import { FakeCheckInRepository } from '#/repositories/fakes/fake-check-ins-repositories'
+import { FakeGymRepository } from '#/repositories/fakes/fake-gyms-repositories'
+import { Decimal } from '@prisma/client/runtime/library'
+
 let checkInsRepository: FakeCheckInRepository
+let gymsRepository: FakeGymRepository
 let sut: CheckInService
 
 describe('Check In Service', () => {
   beforeEach(() => {
     checkInsRepository = new FakeCheckInRepository()
-    sut = new CheckInService(checkInsRepository)
+    gymsRepository = new FakeGymRepository()
+    sut = new CheckInService(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      description: 'Typescript check',
+      title: 'Node Gym',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+      phone: '4812345678',
+    })
+
     vi.useFakeTimers()
   })
 
@@ -20,8 +35,10 @@ describe('Check In Service', () => {
 
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
-      gymId: randomUUID(),
+      gymId: 'gym-01',
       userId: randomUUID(),
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
@@ -33,12 +50,16 @@ describe('Check In Service', () => {
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     await expect(() =>
       sut.execute({
         gymId: 'gym-01',
         userId: 'user-01',
+        userLatitude: 0,
+        userLongitude: 0,
       }),
     ).rejects.toBeInstanceOf(Error)
   })
@@ -49,6 +70,8 @@ describe('Check In Service', () => {
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     vi.setSystemTime(new Date(2022, 7, 21, 12, 0, 0))
@@ -56,6 +79,8 @@ describe('Check In Service', () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
--- api-solid/src/services/check-in.ts
+++ api-solid/src/services/check-in.ts
@@ -1,9 +1,13 @@
 import { ICheckInsRepository } from '#/repositories/check-ins-repository'
+import { IGymsRepository } from '#/repositories/gyms-repository'
 import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface IServiceRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface IServiceResponse {
@@ -11,9 +15,20 @@ interface IServiceResponse {
 }
 
 export class CheckInService {
-  constructor(private checkInsRepository: ICheckInsRepository) {}
+  constructor(
+    private checkInsRepository: ICheckInsRepository,
+    private gymsRepository: IGymsRepository,
+  ) {}
 
   async execute({ userId, gymId }: IServiceRequest): Promise<IServiceResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    /// calculate distanece to-do
+
     const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
